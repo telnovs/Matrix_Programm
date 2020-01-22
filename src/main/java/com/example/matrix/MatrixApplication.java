843d01c66abf08f4009db048646300bb57ae82ba
Telnovs <telnov-s@mai.ru>
2020-01-23 03:32:41 +0700

    New1

---
+++ src/main/java/com/example/matrix/MatrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class MatrixApplication {
+public static void main(String[] args) {
+        SpringApplication.run(MatrixApplication.class, args);
+        }
+        }
