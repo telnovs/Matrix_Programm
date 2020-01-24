80ac94706c04813350195adb3db7bea1b0f95f79
Alexander Kamyhin <kamyhin@gmail.com>
2020-01-24 18:15:00 +0300

    Kamyhin (#10)
    * refactor(Application): Новые зависимости
    
    * feat(Application): Настройки приложения
    
    * feat(Matrix): Добавил модель 'Matrix'
    
    * feat(Matrix): Добавил 'MatrixRepository'
    
    * feat(Matrix): Добавил 'MatrixController'
    
    * feat(Migration): Добавил sql файлы миграций
    
    * feat(Template): Добавил шаблоны вывода
    
    * refactor(Application): gitignore update

---
+++ src/main/resources/db/migration/V1__InitDB.sql
@@ -0,0 +1,10 @@
+create sequence hibernate_sequence start with 1 increment by 1;
+create table matrices
+(
+    id     int8 not null,
+    name   varchar(255),
+    size   integer,
+    primary key (id)
+);
+alter table matrices
+    add constraint UK_gj4cco7y40711xp7icvrfqinf unique (name);
