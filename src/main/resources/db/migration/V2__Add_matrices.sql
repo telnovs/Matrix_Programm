95491bf10329c34d123b5df76c04cc3314a59ab0
Alexander Kamyhin <kamyhin@gmail.com>
2020-01-24 14:43:41 +0300

    feat(Migration): Добавил sql файлы миграций

---
+++ src/main/resources/db/migration/V2__Add_matrices.sql
@@ -0,0 +1,11 @@
+INSERT INTO matrices (id, name, size)
+VALUES (1, 'Test matrix 1', 21),
+       (2, 'Test matrix 2', 12),
+       (3, 'Test matrix 3', 2),
+       (4, 'Test matrix 4', 11),
+       (5, 'Test matrix 5', 32),
+       (6, 'Test matrix 6', 5),
+       (7, 'Test matrix 7', 9),
+       (8, 'Test matrix 8', 10),
+       (9, 'Test matrix 9', 55),
+       (10, 'Test matrix 10', 15);
